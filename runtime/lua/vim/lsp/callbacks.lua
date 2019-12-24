0dc7d58f8195c118ebd3f9b1f80aa713d953d5d3
Jesse Bakker <git@jessebakker.com>
2019-12-24 17:18:17 +0100

    Make workspace/applyEdit callback return a response

--- runtime/lua/vim/lsp/callbacks.lua
+++ runtime/lua/vim/lsp/callbacks.lua
@@ -47,7 +47,11 @@ M['workspace/applyEdit'] = function(_, _, workspace_edit)
   if workspace_edit.label then
     print("Workspace edit", workspace_edit.label)
   end
-  util.apply_workspace_edit(workspace_edit.edit)
+  local status, err = pcall(util.apply_workspace_edit, workspace_edit.edit)
+  return {
+    applied = status;
+    failureReason = err;
+  }
 end
 
 M['textDocument/publishDiagnostics'] = function(_, _, result)
